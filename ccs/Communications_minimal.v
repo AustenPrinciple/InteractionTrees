e36b67d88e66cf96f9dcf30bb1452189672ed782
Yannick <y.zakowski@gmail.com>
2021-04-15 19:06:35 +0200

    Alternate version with no dynamic failure

--- ccs/Communications_minimal.v
+++ ccs/Communications_minimal.v
@@ -10,40 +10,32 @@ Import ITreeNotations.
 Open Scope itree.
 
 From Coq Require Import
+     List
      Strings.String.
 
 (* end hide *)
 
 Open Scope string.
 
+Section Syntax.
+
+  Definition chan : Set := string.
+  Definition pid : Set := string.
+
+  Variant action : Type :=
+  | Send (c : chan) : action
+  | Rcv (c : chan) : action.
+
+  (* We consider CCS with guarded choice for now *)
+  Inductive term : Type :=
+  | DoneT : term
+  | ActionT (a : action) (P : term)
+  | ParaT (P1 P2 : term)
+  | RestrictT (c : chan) (P : term).
+  
+End Syntax.
+
 Module CCS.
-
-  Section Syntax.
-
-    Definition chan : Set := string.
-    Definition pid : Set := string.
-
-    Variant action : Type :=
-    | Send (c : chan) : action
-    | Rcv (c : chan) : action.
-
-    (* We consider CCS with guarded choice for now *)
-    Inductive term : Type :=
-    | NilT : term
-    | ActionT (a : action) (P : term)
-    | ParaT (P1 P2 : term)
-    | RestrictT (c : chan) (P : term).
-
-  End Syntax.
-
-  (* Comment faire un opérateur parallèle qui combine les trois "types" de comportements de (P1 || P2):
-   P1 expose au moinde extérieur une action
-   P2 expose au moinde extérieur une action
-   P1 et P2 font une synchro
-
-   ⟦ P ⟧ : term -> itree E unit/term
-
-   *)
 
   Section Semantics.
 
@@ -58,10 +50,7 @@ Module CCS.
     Defined.
 
     Definition are_opposite (a b : action) : bool :=
-      match action_eq_dec a (op a) with
-      | left _ => true
-      | right _ => false
-      end.
+      if action_eq_dec a (op a) then true else false.
 
     Variant ActionE : Type -> Type :=
     | Act (a : action) : ActionE unit.
@@ -77,11 +66,12 @@ Module CCS.
     Definition dead {A : Type} {E} `{DeadE -< E} : itree E A :=
       x <- trigger (Throw tt);; match x: void with end.
 
-    Notation ccsE := (SchedE +' ActionE +' SynchE +' DeadE).
+    Notation ccsE   := (SchedE +' ActionE +' SynchE +' DeadE).
     Notation ccsT T := (itree ccsE T).
-    Notation ccs := (ccsT unit).
+    Notation ccs    := (ccsT unit).
 
-    Definition nil : ccs := Ret tt.
+    Definition done : ccs := Ret tt.
+
     Definition act (a : action) : ccs := trigger (Act a).
 
     Definition branch (P Q R : ccs) : ccs :=
@@ -168,7 +158,7 @@ Module CCS.
 
     Fixpoint model (t : term) : ccs :=
       match t with
-      | NilT          => nil
+      | DoneT          => done
       | ActionT a t   => act a;; model t
       | ParaT t1 t2   => para (model t1) (model t2)
       | RestrictT c t => restrict c (model t)
@@ -178,3 +168,135 @@ Module CCS.
 
 End CCS.
 
+
+Module CCS2.
+
+  Section Semantics.
+
+    Definition op (a : action) : action :=
+      match a with
+      | Send c => Rcv c
+      | Rcv c => Send c
+      end.
+
+    Definition action_eq_dec : forall (a b : action), {a = b} + {~ a = b}.
+      repeat decide equality.
+    Defined.
+
+    Definition are_opposite (a b : action) : bool :=
+      if action_eq_dec a (op a) then true else false.
+
+    Variant ActionE : Type -> Type :=
+    | Act (a : action) : ActionE unit.
+
+    Variant SynchE : Type -> Type := | Synch : SynchE unit.
+
+    Variant choice := | Left | Right | Synchronize.
+
+    Variant SchedE : Type -> Type :=
+    | Sched2 : SchedE bool 
+    | Sched3 : SchedE choice.
+    
+    Notation ccsE   := (SchedE +' ActionE +' SynchE).
+    Notation ccsT T := (itree ccsE T).
+    Notation ccs    := (ccsT unit).
+
+    Definition done : ccs := Ret tt.
+
+    Definition context := list chan. 
+    Definition in_context (Γ : context) (c : chan) :=
+      in_dec string_dec c Γ.
+    Notation "a ∈ Γ" := (in_context Γ a) (at level 10).
+
+    Definition act Γ (a : action) : option ccs :=
+      match a with
+      | Send c | Rcv c => if c ∈ Γ then Some (trigger (Act a)) else None
+      end.
+    
+    Definition branch2 (P Q : ccs) : ccs :=
+      b <- trigger Sched2;;
+      if b : bool then P else Q.
+
+    Definition branch3 (P Q R : ccs) : ccs :=
+      b <- trigger Sched3;;
+      match b with
+      | Left => P
+      | Right => Q
+      | Synchronize => R
+      end.
+
+    Variant head := | HDone | HSynch (P : ccs) | HAct (a : action) (P : ccs).
+
+    (* Notations for patterns *)
+    Notation "'schedP' e" := (inl1 e) (at level 10).
+    Notation "'actP' e" := (inr1 (inl1 e)) (at level 10).
+    Notation "'synchP' e" := (inr1 (inr1 e)) (at level 10).
+    
+    Definition get_hd : ccs -> ccsT head :=
+      cofix get_hd (P : ccs) := 
+        match observe P with
+        | RetF x => Ret HDone
+        | TauF P => Tau (get_hd P)
+        | @VisF _ _ _ T e k => 
+          match e with
+          | schedP e => vis e (fun x => get_hd (k x))
+          | actP e =>
+            match e in ActionE X return (T = X -> ccsT head) with
+            | Act a => fun (Pf : T = unit) =>
+                        Ret (HAct a (@eq_rect_r _ T (fun T => T -> itree ccsE unit) k unit (eq_sym Pf) tt))
+            end eq_refl
+          | synchP e =>
+            match e in SynchE X return (T = X -> ccsT head) with
+            | Synch => fun (Pf : T = unit) =>
+                        Ret (HSynch (@eq_rect_r _ T (fun T => T -> itree ccsE unit) k unit (eq_sym Pf) tt))
+            end eq_refl
+          end
+        end.
+    
+    Definition para : ccs -> ccs -> ccs :=
+      cofix F (P : ccs) (Q : ccs) :=
+        rP <- get_hd P;; 
+        rQ <- get_hd Q;; 
+        match rP, rQ with
+        | HDone, HDone => done
+        | HDone, _ => Q
+        | _, HDone => P
+        | HAct a P', HAct b Q' =>
+          if are_opposite a b
+          then
+            branch3 (vis (Act a) (fun _ => F P' Q))
+                    (vis (Act b) (fun _ => F P Q'))
+                    (vis Synch   (fun _ => F P' Q'))
+          else
+            branch2 (vis (Act a) (fun _ => F P' Q))
+                    (vis (Act b) (fun _ => F P Q'))
+        | HAct a P', HSynch Q' =>
+          branch2 (vis (Act a) (fun _ => F P' Q))
+                  (vis Synch   (fun _ => F P Q'))
+        | HSynch P', HAct a Q' =>
+          branch2 (vis Synch   (fun _ => F P' Q))
+                  (vis (Act a) (fun _ => F P Q'))
+        | HSynch P', HSynch Q' =>
+          branch2 (vis Synch   (fun _ => F P' Q))
+                  (vis Synch   (fun _ => F P Q'))
+        end.
+
+    Import MonadNotation.
+    Open Scope monad.
+    Fixpoint model (Γ : context) (t : term) : option ccs :=
+      match t with
+      | DoneT         => ret done
+      | ActionT a t   =>
+        P <- act Γ a;;
+        Q <- model Γ t;;
+        ret (ITree.bind P (fun _ => Q))
+      | ParaT t1 t2   =>
+        P <- model Γ t1;;
+        Q <- model Γ t2;;
+        ret (para P Q)
+      | RestrictT c t => model (c :: Γ) t
+      end.
+
+  End Semantics.
+
+End CCS2.
